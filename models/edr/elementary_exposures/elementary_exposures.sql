df2cf2c22f3755f0f856d6678ef9ed495b096a7e
Artem Garmash <artem.garmash@ouraring.com>
2023-10-24 23:08:27 +0300

    Fix elementary_exposures

--- models/edr/elementary_exposures/elementary_exposures.sql
+++ models/edr/elementary_exposures/elementary_exposures.sql
@@ -5,6 +5,8 @@
     unique_key='unique_id',
     full_refresh=elementary.get_config_var('elementary_full_refresh'),
     on_schema_change='sync_all_columns',
+    table_type="iceberg",
+    incremental_strategy=elementary.get_default_incremental_strategy(),
   )
 }}
 
